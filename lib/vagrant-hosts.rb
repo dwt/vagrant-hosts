54a5e2c837b126dd6909eb98ca855c2d475ec591
Martin Häcker <spamfaenger@gmx.de>
2011-07-27 07:33:14 +0200

    moved out hooking into vagrant into the top of the plugin

--- lib/vagrant-hosts.rb
+++ lib/vagrant-hosts.rb
@@ -1,3 +1,9 @@
 require 'vagrant-hosts/vagrant-hosts'
 
-# TODO: move registration logic here
+# TODO: find out if this needs to be in more chains
+[:start, :up, :reload, :resume].each do |each|
+  Vagrant::Action[each].use VagrantHosts::HostsSetupMiddleware
+end
+[:destroy, :suspend].each do |each|
+  Vagrant::Action[each].use VagrantHosts::HostsTeardownMiddleware
+end
--- lib/vagrant-hosts/vagrant-hosts.rb
+++ lib/vagrant-hosts/vagrant-hosts.rb
@@ -99,11 +99,3 @@ module VagrantHosts
     end
   end
 end
-
-# TODO: find out if this needs to be in more chains
-[:start, :up, :reload, :resume].each do |each|
-  Vagrant::Action[each].use VagrantHosts::HostsSetupMiddleware
-end
-[:destroy, :suspend].each do |each|
-  Vagrant::Action[each].use VagrantHosts::HostsTeardownMiddleware
-end
